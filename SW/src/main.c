3741640a08ca574a9d589da49400ddee36d2c1dc
Josh Watts <josh+github@sroz.net>
2018-01-24 23:18:09 -0500

    Minimal main() example with proper initialization

--- SW/src/main.c
+++ SW/src/main.c
@@ -1,5 +1,24 @@
-int main(int argc, char const *argv[])
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+
+#ifdef DEBUG
+//TODO: Trim fat of debug console, else we currently over-shoot flash size by 50%
+#include <fsl_debug_console.c>
+#define DBG PRINTF
+#else
+#define DBG(...)
+#define DBG(...)
+#endif
+
+int main(void)
 {
-	/* code */
-	return 0;
+	/* Init board hardware */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitDebugConsole();
+
+	DBG("%s\n", BOARD_NAME);
+
+	while (1) { }
 }
