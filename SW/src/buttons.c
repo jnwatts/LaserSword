0ea4d8bf24b61e8f759f208e7c08644851c90421
Josh Watts <josh+github@sroz.net>
2018-02-04 18:12:02 -0500

    Buttons: Implement delayed button press logic

--- SW/src/buttons.c
+++ SW/src/buttons.c
@@ -9,4 +9,19 @@ void Buttons_Init(void)
 
 	GPIO_PinInit(BOARD_L1_IN_PORT, BOARD_L1_IN_PIN, &pin_config);
 	GPIO_PinInit(BOARD_L2_IN_PORT, BOARD_L2_IN_PIN, &pin_config);
+	GPIO_PinInit(BOARD_SW1_PORT, BOARD_SW1_PIN, &pin_config);
+}
+
+uint32_t Delay_Button_Value(delay_button_t *delay_button)
+{
+	bool state = delay_button->read_state();
+	if (state) {
+		delay_button->counter++;
+	} else if (delay_button->counter > 0) {
+		uint32_t value = delay_button->counter;
+		delay_button->counter = 0;
+		return value;
+	}
+
+	return 0;
 }
--- SW/src/buttons.h
+++ SW/src/buttons.h
@@ -7,9 +7,19 @@
 
 #include "board.h"
 
+typedef bool (*read_state_fptr)(void);
+
+typedef struct {
+	read_state_fptr read_state;
+	uint32_t counter;
+} delay_button_t;
+
 void Buttons_Init(void);
+
+uint32_t Delay_Button_Value(delay_button_t *delay_button);
 
 static inline bool ReadL1State(void) { return GPIO_PinRead(BOARD_L1_IN_PORT, BOARD_L1_IN_PIN); }
 static inline bool ReadL2State(void) { return GPIO_PinRead(BOARD_L2_IN_PORT, BOARD_L2_IN_PIN); }
+static inline bool ReadSW1State(void) { return GPIO_PinRead(BOARD_SW1_PORT, BOARD_SW1_PIN); }
 
 #endif
