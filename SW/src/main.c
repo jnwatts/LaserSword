230e70d12659dbf55624af52d8986d6979ba73be
Josh Watts <josh+github@sroz.net>
2018-02-04 16:52:29 -0500

    main: Implement 100Hz/10ms logic loop

--- SW/src/main.c
+++ SW/src/main.c
@@ -1,3 +1,5 @@
+#include <fsl_ftm.h>
+
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "board.h"
@@ -11,14 +13,30 @@
 #define DBG(...)
 #endif
 
+#define BOARD_PERIODIC_FTM FTM0
+#define TICKS_PER_10MS (BOARD_FULLSPEED_CORE_CLOCK / 8 * 10/*ms*/)
+ftm_config_t ftm_config;
+
 int main(void)
 {
+	uint32_t status;
+
 	/* Init board hardware */
 	BOARD_InitBootPins();
 	BOARD_InitBootClocks();
 	BOARD_InitDebugConsole();
 
-	DBG("%s\n", BOARD_NAME);
+	FTM_GetDefaultConfig(&ftm_config);
+	ftm_config.prescale = kFTM_Prescale_Divide_8;
+	FTM_Init(BOARD_PERIODIC_FTM, &ftm_config);
+	FTM_SetTimerPeriod(BOARD_PERIODIC_FTM, TICKS_PER_10MS);
 
-	while (1) { }
+	FTM_StartTimer(BOARD_PERIODIC_FTM, kFTM_SystemClock);
+	while (1) {
+		status = FTM_GetStatusFlags(BOARD_PERIODIC_FTM);
+		if (status & kFTM_TimeOverflowFlag) {
+			FTM_ClearStatusFlags(BOARD_PERIODIC_FTM, kFTM_TimeOverflowFlag);
+
+		}
+	}
 }
