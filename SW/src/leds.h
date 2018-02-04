7dafa3f4821df15775374c313387a6e1b6f762e0
Josh Watts <josh+github@sroz.net>
2018-02-04 18:12:14 -0500

    Leds: Mark first & last colors

--- SW/src/leds.h
+++ SW/src/leds.h
@@ -6,6 +6,9 @@ typedef enum {
 	LEDS_RED = (1u<<0),
 	LEDS_BLUE = (1u<<1),
 	LEDS_PURPLE = LEDS_RED | LEDS_BLUE,
+
+	LEDS_FIRST = LEDS_RED,
+	LEDS_LAST = LEDS_PURPLE,
 } leds_color_t;
 
 void Leds_Init(void);
